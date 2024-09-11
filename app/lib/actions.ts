82773b284841f0bf733cbe157e17c55e03471186
yll <yll@gmail.com>
2024-09-11 15:58:21 +0700

    14.2: add Create Invoice Form server-side validation

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -5,23 +5,44 @@ import { sql } from '@vercel/postgres'
 import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
 
+export type State = {
+    errors?: {
+        customerId?: string[];
+        amount?: string[];
+        status?: string[];
+    };
+    message?: string | null;
+}
+
 const FormSchema = z.object({
     id: z.string(),
-    customerId: z.string(),
-    amount: z.coerce.number(),
-    status: z.enum(['pending', 'paid']),
+    customerId: z.string({
+        invalid_type_error: 'Please selectc a customer.',
+    }),
+    amount: z.coerce.number().gt(0, {
+        message: 'Please enter an amount greate than $0.',
+    }),
+    status: z.enum(['pending', 'paid'], {
+        invalid_type_error: 'Please select an invoice status',
+    }),
     date: z.string(),
 })
 
 const CreateInvoice = FormSchema.omit({id: true, date: true})
 
-export async function createInvoice(formData: FormData) {
-    const rawFormData = {
-        customerId: formData.get('customerId'),
+export async function createInvoice(prevState: State, formData: FormData) {
+    const validatedFields = CreateInvoice.safeParse({
+        customerId: formData.get('custmerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
+    })
+    if (!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: 'Missing fields, failed to Create Invoice',
+        }
     }
-    const {customerId, amount, status} = CreateInvoice.parse(rawFormData)
+    const {customerId, amount, status} = validatedFields.data
     const amountInCents = amount * 100
     const date = new Date().toISOString().split('T')[0]
 
