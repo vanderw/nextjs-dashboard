61e1c26eb316e8b9301ec14fc1fa183aa19f16ed
yll <yll@gmail.com>
2024-09-11 14:02:34 +0700

    12.2 Update

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -31,3 +31,20 @@ export async function createInvoice(formData: FormData) {
     revalidatePath('/dashboard/invoices')
     redirect('/dashboard/invoices')
 }
+
+const UpdateInvoice = FormSchema.omit({id: true, date: true})
+
+export async function updateInvoice(id: string, formData: FormData) {
+    const { customerId, amount, status} = UpdateInvoice.parse(
+        Object.fromEntries(formData.entries())
+    )
+    const amountInCents = amount * 100
+    
+    await sql`
+        UPDATE invoices
+        SET customer_id=${customerId}, amount=${amountInCents}, status=${status}
+        WHERE id=${id}
+    `
+    revalidatePath('/dashboard/invoices')
+    redirect('/dashboard/invoices')
+}
